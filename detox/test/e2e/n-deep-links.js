661b187eb418735b5693ce587ff46a17c8abe3d2
erezrokah <erezrokah@users.noreply.github.com>
2017-09-21 16:56:06 +0300

    change tests to reflect escaping bug

--- detox/test/e2e/n-deep-links.js
+++ detox/test/e2e/n-deep-links.js
@@ -1,23 +1,30 @@
 describe('Deep Links', () => {
 
   it('device.launchApp({{newInstance: true, url: url}) should launch app and trigger handling of deep links in app', async () => {
-    const url = 'detoxtesturlscheme://such-string';
-    await device.launchApp({newInstance: true, url: url});
+    const escapedUrl = 'detoxtesturlscheme://such-string?foo=1\\&bar=2';
+    const url = 'detoxtesturlscheme://such-string?foo=1&bar=2';
+    await device.launchApp({newInstance: true, url: escapedUrl});
     await expect(element(by.label(url))).toBeVisible();
   });
 
   it('device.openURL({url: url}) should trigger handling of deep links in app when app is in foreground', async () => {
-    const url = 'detoxtesturlscheme://such-string';
+    const url = 'detoxtesturlscheme://such-string?foo=1&bar=2';
     await device.launchApp({newInstance: true});
-    await device.openURL({url: url});
+    await device.openURL({url});
     await expect(element(by.label(url))).toBeVisible();
   });
 
   it('device.launchApp({url: url}) should trigger handling of deep links in app when app is in background', async () => {
-    const url = 'detoxtesturlscheme://such-string';
+    const url = 'detoxtesturlscheme://such-string?foo=1&bar=2';
     await device.launchApp({newInstance: true});
     await device.sendToHome();
-    await device.launchApp({newInstance: false, url: url});
+    await device.launchApp({newInstance: false, url});
+    await expect(element(by.label(url))).toBeVisible();
+  });
+
+  it('device.launchApp({{newInstance: true, url: url}) should launch app and trigger handling of deep links in app', async () => {
+    const url = 'detoxtesturlscheme://such-string?foo=1&bar=2';
+    await device.launchApp({newInstance: true, url});
     await expect(element(by.label(url))).toBeVisible();
   });
 });
